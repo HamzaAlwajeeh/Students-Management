3d7d0c46c1c1c80087e6ba4d84877e4efc2e4ce1
AymanAltairi73 <aymanaltairi7368@gmail.com>
2025-12-03 20:25:11 +0300

    Remove ndkVersion from Android build configuration and update app logo image

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,6 @@ android {
     // اسم الباكيج الجديد
     namespace = "com.example.almaali_university_center"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
